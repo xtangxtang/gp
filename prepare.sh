aa5d11ba637450c7c66ca9f2a8eb70a232be3fc7
xtangxtang <xi.tang@intel.com>
2023-06-23 14:57:15 +0800

    update

---
+++ prepare.sh
@@ -0,0 +1,5 @@
+pip install openai
+pip install pytorch
+pip install chromadb
+pip install langchain
+pip install unstructured
--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 ./run_get_trade_daily.sh
 ./run_get_capital.sh
 ./run_self_list.sh
+
+git add .
+git commit -m "update"
+git push
