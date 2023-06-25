b1d2c6ec9b113819722f215c68198e7509862b77
xtangxtang <xi.tang@intel.com>
2023-06-25 17:36:36 +0800

    chatgpt+langchain

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -8,15 +8,25 @@ from langchain.document_loaders import DirectoryLoader
 from langchain.document_loaders.csv_loader import CSVLoader
 import os
 
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = "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"
 
 os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
-loader = DirectoryLoader('../weibo/user/', glob='wu2198.csv', loader_cls=CSVLoader)
+
+loader = DirectoryLoader('./', glob='kd.txt')
 documents = loader.load()
-text_splitter = CharacterTextSplitter(chunk_size=400, chunk_overlap=0)
-texts = text_splitter.split_documents(documents)
-embeddings = OpenAIEmbeddings(openai_api_key=os.environ['OPENAI_API_KEY'])
-docsearch = Chroma.from_documents(texts, embeddings)
-qa = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type="stuff", vectorstore=docsearch)
-query = "截止2023-06-21时间对大盘有什么看法"
-qa.run(query)
+
+# 初始化加载器
+text_splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap=0)
+# 切割加载的 document
+split_docs = text_splitter.split_documents(documents)
+
+# 初始化 openai 的 embeddings 对象
+embeddings = OpenAIEmbeddings()
+# 将 document 通过 openai 的 embeddings 对象计算 embedding 向量信息并临时存入 Chroma 向量数据库，用于后续匹配查询
+docsearch = Chroma.from_documents(split_docs, embeddings)
+
+# 创建问答对象
+qa = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type="stuff", vectorstore=docsearch,return_source_documents=True)
+# 进行问答
+result = qa({"query": "科大讯飞今年第三季度收入怎样？"})
+print(result)
