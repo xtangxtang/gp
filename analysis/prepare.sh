a2e20081e78af8fed99661e2764ea22e3495b9c9
xtangxtang <xi.tang@intel.com>
2023-06-27 19:15:54 +0800

    update

--- analysis/prepare.sh
+++
@@ -1,8 +0,0 @@
-pip install openai
-pip install pytorch
-pip install chromadb
-pip install langchain
-pip install unstructured
-pip install tiktoken
-pip install matplotlib
-
---
+++ prepare.sh
@@ -0,0 +1,15 @@
+pip install openai
+pip install pytorch
+pip install chromadb
+pip install langchain
+pip install unstructured
+pip install tiktoken
+pip install matplotlib
+pip install selenium
+pip install fake_useragent
+pip install requests_html
+pip install mechanize
+pip install pyvirtualdisplay
+pip install xvfbwrapper
+apt-get install -y xvfb
+
